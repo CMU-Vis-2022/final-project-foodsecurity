5e32d76403c0586bd1f8fcf472cbf8790665f56f
guanjiecheng <gcheng014@gmail.com>
2022-11-18 22:31:27 -0500

    data

--- src/map.ts
+++ src/map.ts
@@ -1,8 +1,10 @@
 import * as d3 from "d3";
 
+
+
 export function mapChart(){
-    const width = document.body.clientWidth;
-    const height = 300;
+    const width = 1200
+    const height = 700;
 
     const svg = d3
                 .create("svg")
@@ -11,12 +13,22 @@ export function mapChart(){
                 .attr("viewBox", [0, 0, width, height])
                 .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
 
-    var projection = d3.geoMercator();
-    var path = d3.geoPath().projection(projection)
-    var data = d3.json("foodAccess.geojson", (d)=> {
-        console.log(d)
-    });
-    
+
+    var data = d3.json("./src/foodAccessData.json").then(d => {
+        var projection = d3.geoIdentity().reflectY(true).fitSize([width,height],d).translate([width/2,height/3]);
+        //var projection = d3.geoAlbersUsa().fitSize([width,height],d)
+        var path = d3.geoPath().projection(projection)
+        svg.selectAll("path")
+            .data(d.features)
+            .enter()
+            .append("path")
+            .attr('d',path)
+            .attr('fill','#FFFFFF')
+            .attr('transform','scale(2)')
+            .attr("class", "countries");
+            }
+        );
+
     return {
         element: svg.node()!,
       };
