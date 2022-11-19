a274df812a60b235c1629b0d4e6dc64cb8b840c0
guanjiecheng <gcheng014@gmail.com>
2022-11-18 22:49:39 -0500

    addedMap

--- src/map.ts
+++ src/map.ts
@@ -3,20 +3,18 @@ import * as d3 from "d3";
 
 
 export function mapChart(){
-    const width = 1200
-    const height = 700;
+    const width = 1200;
+    const height = 600;
 
     const svg = d3
                 .create("svg")
                 .attr("width", width)
                 .attr("height", height)
                 .attr("viewBox", [0, 0, width, height])
-                .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
 
 
-    var data = d3.json("./src/foodAccessData.json").then(d => {
-        var projection = d3.geoIdentity().reflectY(true).fitSize([width,height],d).translate([width/2,height/3]);
-        //var projection = d3.geoAlbersUsa().fitSize([width,height],d)
+    var data = d3.json("./src/foodAccesData.json").then(d => {
+        var projection = d3.geoIdentity().reflectY(true).fitSize([width,height],d);
         var path = d3.geoPath().projection(projection)
         svg.selectAll("path")
             .data(d.features)
@@ -24,8 +22,7 @@ export function mapChart(){
             .append("path")
             .attr('d',path)
             .attr('fill','#FFFFFF')
-            .attr('transform','scale(2)')
-            .attr("class", "countries");
+            .attr("class", "counties");
             }
         );
 
