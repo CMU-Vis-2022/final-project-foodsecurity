148faafcf708c9700fdd52ea13d8c1f5974b19cb
guanjiecheng <gcheng014@gmail.com>
2022-12-06 14:37:00 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ export default {
     target: ["chrome100", "safari15", "firefox100"],
   },
   // Change this to your repo name
-  base: "/CMU-Vis-2022/final-project-foodsecurity/",
+  base: "/final-project-foodsecurity/",
 };
